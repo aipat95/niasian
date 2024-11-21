62c88f94ab68a90e1a1580277c7abded19aa2c20
Pariyapat Tasdorn <pariyapat.tas@gmail.com>
2024-11-21 19:35:37 +0100

    edit check-in

--- reception/src/pages/dashboard/AddCustomers/CheckIn.jsx
+++ reception/src/pages/dashboard/AddCustomers/CheckIn.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import InputField from "./InputField.jsx";
 import SelectField from "./SelectField.jsx";
 import { useForm } from 'react-hook-form';
-import {useAddCustomerMutation} from "../../../redux/CustomerAPI.js";
+//import {useAddCustomerMutation} from "../../../redux/CustomerAPI.js";
 import Swal from 'sweetalert2';
 
 const CheckIn = () => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const [addBook, {isLoading, isError}] = useAddCustomerMutation()
+    //const [addCustomer, {isLoading, isError}] = useAddCustomerMutation()
     const onSubmit = async (data) => {
 
-        const newBookData = {
+        const newCustomerData = {
             ...data,
         }
         try {
-            await addBook(newBookData).unwrap();
+            await addCustomer(newCustomerData).unwrap();
             Swal.fire({
-                title: "Book added",
-                text: "Your book is uploaded successfully!",
+                title: "Check in",
+                text: "Done",
                 icon: "success",
                 showCancelButton: true,
                 confirmButtonColor: "#3085d6",
@@ -27,94 +27,124 @@ const CheckIn = () => {
             reset();
         } catch (error) {
             console.error(error);
-            alert("Failed to add book. Please try again.")
+            alert("Failed to check in. Please try again.")
         }
 
     }
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
-        if(file) {
-            setimageFile(file);
-            setimageFileName(file.name);
-        }
     }
     return (
         <div className="max-w-lg   mx-auto md:p-6 p-3 bg-white rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">Add New Book</h2>
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">Check In</h2>
 
             {/* Form starts here */}
             <form onSubmit={handleSubmit(onSubmit)} className=''>
-                {/* Reusable Input Field for Title */}
+                {/* Reusable Input Field for name */}
                 <InputField
-                    label="Title"
-                    name="title"
-                    placeholder="Enter book title"
+                    label="First Name"
+                    name="firstname"
+                    //placeholder="Enter name"
                     register={register}
                 />
 
-                {/* Reusable Textarea for Description */}
                 <InputField
-                    label="Description"
-                    name="description"
-                    placeholder="Enter book description"
+                    label="Last Name"
+                    name="lastname"
+                    register={register}
+                />
+
+                <InputField
+                    label="ID / Passport Number"
+                    name="id"
                     type="textarea"
                     register={register}
 
                 />
 
-                {/* Reusable Select Field for Category */}
-                <SelectField
-                    label="Category"
-                    name="category"
-                    options={[
-                        { value: '', label: 'Choose A Category' },
-                        { value: 'business', label: 'Business' },
-                        { value: 'technology', label: 'Technology' },
-                        { value: 'fiction', label: 'Fiction' },
-                        { value: 'horror', label: 'Horror' },
-                        { value: 'adventure', label: 'Adventure' },
-                        // Add more options as needed
-                    ]}
-                    register={register}
-                />
-
                 {/* Trending Checkbox */}
-                <div className="mb-4">
+
+                {/*<div className="mb-4">
                     <label className="inline-flex items-center">
                         <input
                             type="checkbox"
                             {...register('trending')}
                             className="rounded text-blue-600 focus:ring focus:ring-offset-2 focus:ring-blue-500"
                         />
-                        <span className="ml-2 text-sm font-semibold text-gray-700">Trending</span>
+                        <span className="ml-2 text-sm font-semibold text-gray-700">Rental</span>
                     </label>
+                </div>*/}
+
+                {/* Reusable Select Field for Category */}
+                <SelectField
+                    label="Tent"
+                    name="tent"
+                    options={[
+                        {value: 'no-rent', label: 'none'},
+                        {value: 'sizeS', label: 'size S (1 adult)'},
+                        {value: 'sizeM', label: 'size M (2 adults)'},
+                        {value: 'sizeXL', label: 'size XL (4 adults)'},
+                        {value: 'sizeFamily', label: 'Family (2 adults, 1-2 children)'},
+                        // Add more options as needed
+                    ]}
+                    register={register}
+                />
+
+
+                <div className='grid md:grid-cols-2 xl:grid-cols-2 xl:grid-rows-2 xl:grid-flow-col gap-x-2'>
+                    <InputField
+                        label="Pillow"
+                        name="pillow"
+                        type="number"
+                        register={register}
+
+                    />
+                    <InputField
+                        label="Blanket"
+                        name="blanket"
+                        type="number"
+                        register={register}
+
+                    />
+                    <InputField
+                        label="Mattress"
+                        name="mattress"
+                        type="number"
+                        register={register}
+
+                    />
+                    <InputField
+                        label="Bicycle"
+                        name="bicycle"
+                        type="number"
+                        register={register}
+
+                    />
+
                 </div>
 
-                {/* Old Price */}
                 <InputField
-                    label="Old Price"
-                    name="oldPrice"
-                    type="number"
-                    placeholder="Old Price"
+                    label="Check-In date"
+                    name="checkindate"
+                    type="date"
                     register={register}
 
                 />
 
-                {/* New Price */}
                 <InputField
-                    label="New Price"
-                    name="newPrice"
-                    type="number"
-                    placeholder="New Price"
+                    label="Check-Out Date"
+                    name="checkoutdate"
+                    type="date"
                     register={register}
 
                 />
 
                 {/* Submit Button */}
                 <button type="submit" className="w-full py-2 bg-green-500 text-white font-bold rounded-md">
+                    ok
                     {
-                        isLoading ? <span className="">Adding.. </span> : <span>Add Book</span>
+                        // isLoading ? <span className="">Adding.. </span> : <span>Add Book</span>
                     }
                 </button>
             </form>
