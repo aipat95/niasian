b09b150acbae373b3eac3c719230c1531b807590
methea15 <154459220+methea15@users.noreply.github.com>
2024-12-07 15:00:11 +0100

    Delete AdminBackend directory

--- AdminBackend/Admin/src/main/java/com/niasian/Admin/respository/ActivityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.niasian.Admin.respository;
-
-import com.niasian.Admin.entity.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity, String> {}
