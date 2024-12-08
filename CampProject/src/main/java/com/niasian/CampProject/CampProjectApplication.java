ccb57448afb265a0de6c5964d9e5680bd0d101d1
Gabrielphoebe <gabrielphoebe1581@gmail.com>
2024-12-08 11:44:05 +0100

    receptionlogin

--- CampProject/src/main/java/com/niasian/CampProject/CampProjectApplication.java
+++ CampProject/src/main/java/com/niasian/CampProject/CampProjectApplication.java
@@ -2,10 +2,8 @@ package com.niasian.CampProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 public class CampProjectApplication {
 
 	public static void main(String[] args) {
